2cb2f7c369646c4bf6d3deba1d5ead350d9238c1
jackmorrison <jsm1317@ic.ac.uk>
2021-04-05 16:01:10 +0100

    Switches local port to not clash with frontend

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const listEndpoints = require("express-list-endpoints");
 var express = require("express"),
   app = express();
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("listening on 3000");
+app.listen(process.env.PORT || 4000, function () {
+  console.log("listening on 4000");
 });
 
 app.use(

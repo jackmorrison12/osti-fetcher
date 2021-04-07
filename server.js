5e01b8ebb9e573a128d6b258cf5f9703d7d20c3c
jackmorrison <jsm1317@ic.ac.uk>
2021-04-08 00:55:04 +0100

    Adds back in music fetching

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.post("/setup", async function (req, res) {
   if (current_status == "api_linked") {
     res.json("fetching_lastfm");
     await UserController.setStatus(req.body.user_id, "fetching_lastfm");
-    // await MusicController.userSetup(req.body.user_id);
+    await MusicController.userSetup(req.body.user_id);
     await UserController.setStatus(req.body.user_id, "fetching_googlefit");
     await FitnessController.userSetup(req.body.user_id);
     await UserController.setStatus(req.body.user_id, "fetched");
